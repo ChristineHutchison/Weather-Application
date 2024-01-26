a12f2cc4f3421a8ffc209515fa98ce8ba61af1ad
ChristineHutchison <149682071+ChristineHutchison@users.noreply.github.com>
2024-01-26 14:43:56 +0000

    Added windspeed, humidity and time elements to the search engine in JS

--- src/index.js
+++ src/index.js
@@ -5,10 +5,35 @@ function refreshWeather(response) {
 	let temperature = Math.round(response.data.temperature.current);
 	let cityElement = document.querySelector("#weather-app-city");
 	let descriptionElement = document.querySelector("#description");
+	let humidityElement = document.querySelector("#humidity");
+	let windspeedElement = document.querySelector("#wind-speed");
+	let timeElement = document.querySelector("#time");
+	let date = new Date(response.data.time * 1000);
 
 	cityElement.innerHTML = response.data.city;
 	todayTemperatureElement.innerHTML = temperature;
 	descriptionElement.innerHTML = response.data.condition.description;
+	humidityElement.innerHTML = response.data.temperature.humidity;
+	windspeedElement.innerHTML = response.data.wind.speed;
+	timeElement.innerHTML = formatDate(date);
+	console.log(response.data);
+}
+
+function formatDate(date) {
+	let minutes = date.getMinutes();
+	let hours = date.getHours();
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+
+	return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -28,4 +53,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Chesterfield");
+searchCity("London");
