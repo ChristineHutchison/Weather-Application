fa3feadf9de14ade6ac2fb78fe0b52d7421fc262
ChristineHutchison <149682071+ChristineHutchison@users.noreply.github.com>
2024-01-26 20:17:28 +0000

    Added js to change todays weather icon

--- src/index.js
+++ src/index.js
@@ -9,14 +9,15 @@ function refreshWeather(response) {
 	let windspeedElement = document.querySelector("#wind-speed");
 	let timeElement = document.querySelector("#time");
 	let date = new Date(response.data.time * 1000);
+	let iconTodayElement = document.querySelector("#todays-weather-icon");
 
+	iconTodayElement.innerHTML = `<img src="${response.data.condition.icon_url}" id="todays-weather-icon"/>`;
 	cityElement.innerHTML = response.data.city;
 	todayTemperatureElement.innerHTML = temperature;
 	descriptionElement.innerHTML = response.data.condition.description;
 	humidityElement.innerHTML = response.data.temperature.humidity;
 	windspeedElement.innerHTML = response.data.wind.speed;
 	timeElement.innerHTML = formatDate(date);
-	console.log(response.data);
 }
 
 function formatDate(date) {
@@ -33,15 +34,17 @@ function formatDate(date) {
 	];
 	let day = days[date.getDay()];
 
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
 	return `${day} ${hours}:${minutes}`;
 }
-
 function searchCity(city) {
 	let apiKey = "bc4649524ebb028af8032a47bt4c53o0";
 	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(refreshWeather);
 }
-
 function handleSearchSubmit(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector("#search-form-input");
@@ -49,7 +52,6 @@ function handleSearchSubmit(event) {
 	cityElement.innerHTML = searchInput.value;
 	searchCity(searchInput.value);
 }
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
