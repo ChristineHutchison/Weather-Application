042027f43ce8c9dda45280b09c43b03a85de0cc3
ChristineHutchison <149682071+ChristineHutchison@users.noreply.github.com>
2024-02-04 22:00:10 +0000

    Added JS to forecast

--- src/index.js
+++ src/index.js
@@ -55,4 +55,28 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function displayForecast() {
+	let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+	let forecastHtml = "";
+
+	days.forEach(function (day) {
+		forecastHtml =
+			forecastHtml +
+			`
+			<div class="weather-forecast-day">
+						<div class="weather-forecast-date">${day}</div>
+						<div class="weather-forecast-icon">☀️</div>
+						<div class="weather-forecast-temperatures">
+							<span class="weather-forecast-temperature-max">18°</span>
+							<span class="weather-forecast-temperature-min">12°</span>
+						</div>
+					</div>
+				`;
+	});
+
+	let forecastElement = document.querySelector("#forecast");
+	forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("London");
+displayForecast();
